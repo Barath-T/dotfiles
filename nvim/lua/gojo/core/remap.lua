fc2e556aebd90e0f2c0955a36447933025deb0bc
Barath T <barath1272003@outlook.com>
2024-01-20 19:47:52 +0530

    removed waste yank remap

--- nvim/lua/gojo/core/remap.lua
+++ nvim/lua/gojo/core/remap.lua
@@ -9,7 +9,6 @@ vim.keymap.set("n", "N", "Nzzz")
 
 vim.keymap.set("n", "<leader>y", "\"+y")
 vim.keymap.set("v", "<leader>y", "\"+y")
-vim.keymap.set("n", "<leader>Y", "\"+Y")
 
 vim.keymap.set("n", "Q", "<nop>")
 
--- nvim/lua/gojo/plugins/lsp.lua
+++ nvim/lua/gojo/plugins/lsp.lua
@@ -1,3 +1,4 @@
+--return {}
 return {
   --- Uncomment the two plugins below if you want to manage the language servers from neovim
   --- and read this: https://github.com/VonHeikemen/lsp-zero.nvim/blob/v3.x/doc/md/guides/integrate-with-mason-nvim.md
